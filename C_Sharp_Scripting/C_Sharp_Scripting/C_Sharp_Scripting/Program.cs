e7bcdb3da04874cd152319a7e1f0eaedd337bf41
philippfuereder <43365816+philippfuereder@users.noreply.github.com>
2022-11-01 18:38:50 +0100

    c#-scripting

---
+++ C_Sharp_Scripting/C_Sharp_Scripting/C_Sharp_Scripting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_Scripting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnNumber());
+            Console.ReadKey();
+        }
+        public static int ReturnNumber()
+        {
+            return 42;
+        }
+    }
+}
+
+
+
