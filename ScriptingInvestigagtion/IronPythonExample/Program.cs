f102d57054f6fd67726b091ad237fcd237cbfbf5
unknown <r.superfreisi@gmail.com>
2022-11-27 00:28:22 +0100

    Add IronPythonExample

---
+++ ScriptingInvestigagtion/IronPythonExample/Program.cs
@@ -0,0 +1,8 @@
+﻿using IronPython.Hosting;
+
+var engine = Python.CreateEngine();
+var scope = engine.CreateScope();
+var source = engine.CreateScriptSourceFromFile("test.py");
+source.Execute(scope);
+var test = scope.GetVariable("test");
+Console.WriteLine(test);
---
+++ ScriptingInvestigagtion/IronPythonExample/test.py
@@ -0,0 +1,5 @@
+﻿def function():
+	return 42
+
+
+test = function()
