fa83c4dd94cd4d4959b185be69f7d54a5a4ff757
unknown <r.superfreisi@gmail.com>
2022-11-30 12:15:24 +0100

    Updated DotNetBenchmark

--- ScriptingInvestigagtion/IronPythonVsLua/IronPythonVsLua.cs
+++ ScriptingInvestigagtion/IronPythonVsLua/IronPythonVsLua.cs
@@ -1,27 +1,99 @@
 ﻿using BenchmarkDotNet.Attributes;
 using IronPython.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Scripting.Hosting;
+using NLua;
 
 namespace IronPythonVsLua
 {
     [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
     public class IronPythonVsLua
     {
+        #region IronPython
         [Benchmark]
-        public void TestIronPython() => IronPython();
+        public void TestIronPython() => IronPythonSimple();
+        [Benchmark]
+        public void TestIronPythonSum() => IronPythonSum();
+        [Benchmark]
+        public void TestIronPythonPassDotNetObjectsAndCallFunction() => IronPythonPassDotNetObjectsAndCallFunction();
+       
+        #endregion
 
-        public void IronPython()
+        private readonly Lua state = new Lua();
+        private readonly ScriptEngine engine = Python.CreateEngine();
+        #region Lua
+        [Benchmark]
+        public void TestLua() => LuaSimple();
+        [Benchmark]
+        public void TestLuaSum() => LuaSum();
+        [Benchmark]
+        public void TestLuaPassDotNetObjectsAndCallFunction() => LuaPassDotNetObjectAndCallFunction();
+        #endregion
+
+        #region IronPythonMethods
+        public void IronPythonSimple()
         {
-            var engine = Python.CreateEngine();
             var scope = engine.CreateScope();
-            var source = engine.CreateScriptSourceFromString("def function():\r\n\treturn 42\r\n\r\n\r\ntest = function()");
+            var source = engine.CreateScriptSourceFromString("def fun():\r\n\treturn 42\r\n\r\n\r\ntest = fun()");
             source.Execute(scope);
             var test = scope.GetVariable("test");
             Console.WriteLine(test);
         }
+
+        public void IronPythonSum()
+        {
+            var scope = engine.CreateScope();
+            var source = engine.CreateScriptSourceFromString("def sum(x,y): \r\n\t return x+ y\r\n\r\n\r\n result=sum(3,3)");
+            source.Execute(scope);
+            var result = scope.GetVariable("result");
+            Console.WriteLine(result);
+        }
+
+        public void IronPythonPassDotNetObjectsAndCallFunction() 
+        {
+            var scope = engine.CreateScope();
+            var obj = new SomeClass();
+            scope.SetVariable("obj", obj);
+            var source = engine.CreateScriptSourceFromString(SetPythonScript());
+            source.Execute(scope);
+            var result = scope.GetVariable("result");
+            Console.WriteLine(result);
+        }
+        #endregion
+
+        #region LuaMethods
+        public void LuaPassDotNetObjectAndCallFunction()
+        {
+            var obj = new SomeClass();
+            state["obj"] = obj;
+            state.DoString (@"result=obj:Func1()");
+            var result = state["result"];
+            Console.WriteLine(result);
+        }
+
+        private static string SetPythonScript()
+        {
+            string s = "";
+            s += "import clr" + "\r\n";
+            s += "result=obj.Func1() " + "\r\n";
+            return s;
+        }
+
+        public void LuaSimple()
+        {
+            state.DoString("function fun() \r\n\t  return 42 \r\n end \r\n test=fun()");
+            var test = state["test"];
+            Console.WriteLine(test);
+        }
+
+        public void LuaSum()
+        {
+
+
+            state.DoString("function sum(x,y) \r\n\t return x+y \r\n end \r\n result=sum(3,3)");
+            var result = state["result"];
+            Console.WriteLine(result);
+        }
+
+        #endregion
     }
 }
---
+++ ScriptingInvestigagtion/IronPythonVsLua/SomeClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronPythonVsLua
+{
+    public class SomeClass
+    {
+        public string MyProperty { get; private set; }
+
+        public SomeClass(string param1 = "defaulValue")
+        {
+            MyProperty = param1;
+        }
+
+        public int Func1()
+        {
+            return 32;
+        }
+
+        public string AnotherFunc(int val1, string val2)
+        {
+            return "Some String";
+        }
+
+        public static string StaticMethod(int param)
+        {
+            return "Return of Static Method";
+        }
+    }
+}
